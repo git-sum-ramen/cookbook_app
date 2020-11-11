70162bb3bb2fd51f2a9339c7c2bf18746d2ac26a
brian <riceb53@gmail.com>
2020-11-10 21:56:30 -0500

    add ingredients_list model method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    # p "i am in the ingredietns list method"    
+    ingredients.split(", ").map {|ingredient| ingredient.capitalize.tr('.', '')}
+  end
 end
