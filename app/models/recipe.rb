3a7bea95e71d608647b5071cdf240cefc07ba1ce
brian <riceb53@gmail.com>
2020-11-10 22:27:33 -0500

    add additional model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,18 @@ class Recipe < ApplicationRecord
     # p "i am in the ingredietns list method"    
     ingredients.split(", ").map {|ingredient| ingredient.capitalize.tr('.', '')}
   end
+
+  def directions_list
+    directions.split(", ").map {|ingredient| ingredient.capitalize.tr('.', '')}
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B, %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    "#{hours} hours #{minutes} minutes"
+  end
 end
