aeb432a9504f9992ecc6514a4e5edaca46173667
brian <riceb53@gmail.com>
2021-01-07 19:33:07 -0500

    modify seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :recipes
-  validates :name, length: {maximum: 20}
-  validates :password, length: { in: 6..20 }
+  # validates :name, length: {maximum: 20}
+  # validates :password, length: { in: 6..20 }
 end
